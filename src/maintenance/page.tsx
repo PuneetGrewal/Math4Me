cecd2389ca21755bdf9c9f14200db14f5d708f77
Puneet Grewal <puneetsgrewal@gmail.com>
2025-02-10 11:53:11 -0800

    rand

---
+++ src/maintenance/page.tsx
@@ -0,0 +1,9 @@
+export default function MaintenancePage() {
+    return (
+      <div style={{ textAlign: 'center', padding: '50px' }}>
+        <h1>🚧 Site Under Maintenance 🚧</h1>
+        <p>We are currently updating our website. Please check back soon!</p>
+      </div>
+    );
+  }
+  
